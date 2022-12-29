8d7eba8d5507ec94bccc0bcae82cfd8fe1156545
Akilah Powell <atpjun@gmail.com>
2022-12-29 00:25:40 -0400

    Revert "almost there"
    This reverts commit 288652cc6181d1db89cec5dc8ad78d4b622d37d7.

--- profile-feed/profile-feed.js
+++ profile-feed/profile-feed.js
@@ -6,7 +6,7 @@ const postSectionsEl = document.getElementById('posts-section');
 const avatarImgEl = document.querySelector('#avatar-img');
 const usernameHeaderEl = document.querySelector('#username-header');
 const headlineHeaderEl = document.querySelector('#headline-header');
-const profileLikesEl = document.querySelector('#profile-likes');
+const profileLikesEl = document.querySelector('.profile-likes');
 
 
 
@@ -65,14 +65,14 @@ export async function displayPosts() {
 
 function renderLikes({ likes, id }) {
     const likeButton = document.createElement('button');
+    const div = document.createElement('div');
 
 
-
-
+    div.classList.add('profile-likes');
     likeButton.classList.add('like-button');
-    likeButton.textContent = `${likes}🍃`;
+    likeButton.textContent = `Likes ${likes}🍃`;
 
-
+    div.append(likeButton);
 
 
 
@@ -81,7 +81,7 @@ function renderLikes({ likes, id }) {
         await displayProfile();
     });
 
-    return likeButton;
+    return div;
 }
 
 export function renderImageNav() {
